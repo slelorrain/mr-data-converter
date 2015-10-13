b18d597e18d444e1453dcc5e97165c0ea51dfe7d
Tom Doan <tomdoan@singpost.com>
2015-10-13 15:26:47 +0800

    Optimize code

--- js/CSVParser.js
+++ js/CSVParser.js
@@ -6,162 +6,153 @@
 //
 //  CSV Parsing Function from Ben Nadel, http://www.bennadel.com/blog/1504-Ask-Ben-Parsing-CSV-Strings-With-Javascript-Exec-Regular-Expression-Command.htm
 
-
-var isDecimal_re     = /^\s*(\+|-)?((\d+([,\.]\d+)?)|([,\.]\d+))\s*$/;
-
-var CSVParser = {
+var isDecimal_re = /^\s*(\+|-)?((\d+([,\.]\d+)?)|([,\.]\d+))\s*$/,
+  CSVParser = {
 
   //---------------------------------------
   // UTILS
   //---------------------------------------
 
   isNumber: function(string) {
-    if( (string == null) || isNaN( new Number(string) ) ) {
+    if (string === null || isNaN(+string)) {
       return false;
     }
     return true;
   },
-
 
   //---------------------------------------
   // PARSE
   //---------------------------------------
   //var parseOutput = CSVParser.parse(this.inputText, this.headersProvided, this.delimiter, this.downcaseHeaders, this.upcaseHeaders);
 
-  parse: function (input, headersIncluded, delimiterType, downcaseHeaders, upcaseHeaders, decimalSign) {
+  parse: function(input, headersIncluded, delimiterType, downcaseHeaders, upcaseHeaders, decimalSign) {
+    var dataArray = [],
+      errors = [];
 
-    var dataArray = [];
+    // Test for delimiter
+    // Count the number of commas
+    var RE = new RegExp('[^,]', 'gi'),
+      numCommas = input.replace(RE, '').length;
 
-    var errors = [];
+    // Count the number of tabs
+    RE = new RegExp('[^\t]', 'gi');
+    var numTabs = input.replace(RE, '').length;
 
-    //test for delimiter
-    //count the number of commas
-    var RE = new RegExp("[^,]", "gi");
-    var numCommas = input.replace(RE, "").length;
-
-    //count the number of tabs
-    RE = new RegExp("[^\t]", "gi");
-    var numTabs = input.replace(RE, "").length;
-
-    var rowDelimiter = "\n";
-    //set delimiter
-    var columnDelimiter = ",";
+    // Set delimiter
+    var rowDelimiter = '\n',
+      columnDelimiter = ',';
     if (numTabs > numCommas) {
-      columnDelimiter = "\t"
-    };
-
-    if (delimiterType === "comma") {
-      columnDelimiter = ","
-    } else if (delimiterType === "tab") {
-      columnDelimiter = "\t"
+      columnDelimiter = '\t';
     }
 
+    if (delimiterType === 'comma') {
+      columnDelimiter = ',';
+    } else if (delimiterType === 'tab') {
+      columnDelimiter = '\t';
+    }
 
-    // kill extra empty lines
-    RE = new RegExp("^" + rowDelimiter + "+", "gi");
-    input = input.replace(RE, "");
-    RE = new RegExp(rowDelimiter + "+$", "gi");
-    input = input.replace(RE, "");
+    // Kill extra empty lines
+    RE = new RegExp('^' + rowDelimiter + '+', 'gi');
+    input = input.replace(RE, '');
+    RE = new RegExp(rowDelimiter + '+$', 'gi');
+    input = input.replace(RE, '');
 
-    // var arr = input.split(rowDelimiter);
-    //
-    // for (var i=0; i < arr.length; i++) {
-    //   dataArray.push(arr[i].split(columnDelimiter));
-    // };
+    //var arr = input.split(rowDelimiter);
+    //for (var i=0; i<arr.length; i++) {
+    //  dataArray.push(arr[i].split(columnDelimiter));
+    //}
 
-
-    // dataArray = jQuery.csv(columnDelimiter)(input);
+    //dataArray = jQuery.csv(columnDelimiter)(input);
     dataArray = this.CSVToArray(input, columnDelimiter);
 
-    //escape out any tabs or returns or new lines
-    for (var i = dataArray.length - 1; i >= 0; i--){
-      for (var j = dataArray[i].length - 1; j >= 0; j--){
-        dataArray[i][j] = dataArray[i][j].replace("\t", "\\t");
-        dataArray[i][j] = dataArray[i][j].replace("\n", "\\n");
-        dataArray[i][j] = dataArray[i][j].replace("\r", "\\r");
-      };
-    };
-
-
-    var headerNames = [];
-    var headerTypes = [];
-    var numColumns = dataArray[0].length;
-    var numRows = dataArray.length;
-    if (headersIncluded) {
-
-      //remove header row
-      headerNames = dataArray.splice(0,1)[0];
-      numRows = dataArray.length;
-
-    } else { //if no headerNames provided
-
-      //create generic property names
-      for (var i=0; i < numColumns; i++) {
-        headerNames.push("val"+String(i));
-        headerTypes.push("");
-      };
-
-    }
-
-
-    if (upcaseHeaders) {
-      for (var i = headerNames.length - 1; i >= 0; i--){
-        headerNames[i] = headerNames[i].toUpperCase();
-      };
-    };
-    if (downcaseHeaders) {
-      for (var i = headerNames.length - 1; i >= 0; i--){
-        headerNames[i] = headerNames[i].toLowerCase();
-      };
-    };
-
-    //test all the rows for proper number of columns.
-    for (var i=0; i < dataArray.length; i++) {
-      var numValues = dataArray[i].length;
-      if (numValues != numColumns) {this.log("Error parsing row "+String(i)+". Wrong number of columns.")};
-    };
-
-    //test columns for number data type
-    var numRowsToTest = dataArray.length;
-    var threshold = 0.9;
-    for (var i=0; i < headerNames.length; i++) {
-      var numFloats = 0;
-      var numInts = 0;
-      for (var r=0; r < numRowsToTest; r++) {
-        if (dataArray[r]) {
-          //replace comma with dot if comma is decimal separator
-          if(decimalSign='comma' && isDecimal_re.test(dataArray[r][i])){
-            dataArray[r][i] = dataArray[r][i].replace(",", ".");
-          }
-          if (CSVParser.isNumber(dataArray[r][i])) {
-            numInts++
-            if (String(dataArray[r][i]).indexOf(".") > 0) {
-              numFloats++
-            }
-          };
-        };
-
-      };
-
-      if ((numInts / numRowsToTest) > threshold){
-        if (numFloats > 0) {
-          headerTypes[i] = "float"
-        } else {
-          headerTypes[i] = "int"
-        }
-      } else {
-        headerTypes[i] = "string"
+    // Escape out any tabs or returns or new lines
+    for (var i=dataArray.length-1; i>=0; --i) {
+      for (var j=dataArray[i].length-1; j>=0; --j) {
+        dataArray[i][j] = dataArray[i][j].replace('\t', '\\t');
+        dataArray[i][j] = dataArray[i][j].replace('\n', '\\n');
+        dataArray[i][j] = dataArray[i][j].replace('\r', '\\r');
       }
     }
 
+    var headerNames = [],
+      headerTypes = [],
+      numColumns = dataArray[0].length,
+      numRows = dataArray.length;
 
+    if (headersIncluded) {
+      // Remove header row
+      headerNames = dataArray.splice(0,1)[0];
+      numRows = dataArray.length;
+    } else { // If no headerNames provided
+      // Create generic property names
+      for (var i=0; i<numColumns; ++i) {
+        headerNames.push('val' + String(i));
+        headerTypes.push('');
+      }
+    }
 
+    if (upcaseHeaders) {
+      for (var i=headerNames.length-1; i>=0; --i) {
+        headerNames[i] = headerNames[i].toUpperCase();
+      }
+    }
+    if (downcaseHeaders) {
+      for (var i=headerNames.length-1; i>=0; --i) {
+        headerNames[i] = headerNames[i].toLowerCase();
+      }
+    }
 
+    // Test all the rows for proper number of columns.
+    for (var i=0, imax=dataArray.length; i<imax; ++i) {
+      var numValues = dataArray[i].length;
+      if (numValues !== numColumns) {
+        this.log('Error parsing row ' + String(i) + '. Wrong number of columns.');
+      }
+    }
 
-    return {'dataGrid':dataArray, 'headerNames':headerNames, 'headerTypes':headerTypes, 'errors':this.getLog()}
+    // Test columns for number data type
+    var numRowsToTest = dataArray.length,
+      threshold = 0.9;
+
+    for (var i=0, imax=headerNames.length; i<imax; ++i) {
+      var numFloats = 0,
+        numInts = 0;
+
+      for (var r=0; r<numRowsToTest; ++r) {
+        if (dataArray[r]) {
+          //replace comma with dot if comma is decimal separator
+          if (decimalSign='comma' && isDecimal_re.test(dataArray[r][i])) {
+            dataArray[r][i] = dataArray[r][i].replace(',', '.');
+          }
+          if (CSVParser.isNumber(dataArray[r][i])) {
+            ++numInts;
+            if (String(dataArray[r][i]).indexOf('.') > 0) {
+              ++numFloats;
+            }
+          }
+        }
+
+      }
+
+      if ((numInts / numRowsToTest) > threshold) {
+        if (numFloats > 0) {
+          headerTypes[i] = 'float';
+        } else {
+          headerTypes[i] = 'int';
+        }
+      } else {
+        headerTypes[i] = 'string';
+      }
+    }
+
+    return {
+      'dataGrid': dataArray,
+      'headerNames': headerNames,
+      'headerTypes': headerTypes,
+      'errors': this.getLog()
+    };
 
   },
-
 
   //---------------------------------------
   // ERROR LOGGING
@@ -177,18 +168,16 @@ var CSVParser = {
   },
 
   getLog: function() {
-    var out = "";
+    var out = '';
     if (this.errorLog.length > 0) {
-      for (var i=0; i < this.errorLog.length; i++) {
-        out += ("!!"+this.errorLog[i] + "!!\n");
-      };
-      out += "\n"
-    };
+      for (var i=0, imax=this.errorLog.length; i<imax; ++i) {
+        out += ('!!' + this.errorLog[i] + '!!\n');
+      }
+      out += '\n';
+    }
 
     return out;
   },
-
-
 
   //---------------------------------------
   // UTIL
@@ -198,26 +187,24 @@ var CSVParser = {
     // This will parse a delimited string into an array of
     // arrays. The default delimiter is the comma, but this
     // can be overriden in the second argument.
-    CSVToArray: function( strData, strDelimiter ){
+    CSVToArray: function(strData, strDelimiter) {
       // Check to see if the delimiter is defined. If not,
       // then default to comma.
-      strDelimiter = (strDelimiter || ",");
+      strDelimiter = (strDelimiter || ',');
 
       // Create a regular expression to parse the CSV values.
       var objPattern = new RegExp(
         (
-          // Delimiters.
-          "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
-
-          // Quoted fields.
-          "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
-
-          // Standard fields.
-          "([^\"\\" + strDelimiter + "\\r\\n]*))"
+          /* Delimiters */
+          '(\\' + strDelimiter + '|\\r?\\n|\\r|^)' +
+          /* Quoted fields */
+          '(?:"([^"]*(?:""[^"]*)*)"|' +
+          /* Standard fields */
+          '([^"\\' + strDelimiter + '\\r\\n]*))'
         ),
-        "gi"
+          /* Global, case insensitive */
+          'gi'
         );
-
 
       // Create an array to hold our data. Give the array
       // a default empty first row.
@@ -227,59 +214,42 @@ var CSVParser = {
       // matching groups.
       var arrMatches = null;
 
-
       // Keep looping over the regular expression matches
       // until we can no longer find a match.
-      while (arrMatches = objPattern.exec( strData )){
+      while (arrMatches = objPattern.exec(strData)) {
 
         // Get the delimiter that was found.
-        var strMatchedDelimiter = arrMatches[ 1 ];
+        var strMatchedDelimiter = arrMatches[1];
 
         // Check to see if the given delimiter has a length
         // (is not the start of string) and if it matches
         // field delimiter. If id does not, then we know
         // that this delimiter is a row delimiter.
-        if (
-          strMatchedDelimiter.length &&
-          (strMatchedDelimiter != strDelimiter)
-          ){
-
+        if (strMatchedDelimiter.length && (strMatchedDelimiter !== strDelimiter)) {
           // Since we have reached a new row of data,
           // add an empty row to our data array.
-          arrData.push( [] );
-
+          arrData.push([]);
         }
-
 
         // Now that we have our delimiter out of the way,
         // let's check to see which kind of value we
         // captured (quoted or unquoted).
-        if (arrMatches[ 2 ]){
-
+        if (arrMatches[2]) {
           // We found a quoted value. When we capture
           // this value, unescape any double quotes.
-          var strMatchedValue = arrMatches[ 2 ].replace(
-            new RegExp( "\"\"", "g" ),
-            "\""
-            );
-
+          var strMatchedValue = arrMatches[2].replace(/""/g, '"');
         } else {
-
           // We found a non-quoted value.
-          var strMatchedValue = arrMatches[ 3 ];
-
+          var strMatchedValue = arrMatches[3];
         }
-
 
         // Now that we have our value string, let's add
         // it to the data array.
-        arrData[ arrData.length - 1 ].push( strMatchedValue );
+        arrData[arrData.length-1].push(strMatchedValue);
       }
 
       // Return the parsed data.
-      return( arrData );
+      return arrData;
     }
 
-
-
-}
+  };
--- js/Controller.js
+++ js/Controller.js
@@ -1,80 +1,71 @@
 var _gaq = _gaq || [];
 
-$(document).ready(function(){
-  var widthOffset = 345;
-  var heightOffset = 35
-
-  var d = new DataConverter('converter');
-
-  var sidebar = $('#header');
-
-  var win = $(window);
-  var w = win.width() - widthOffset;
-  var h = win.height() - heightOffset;
+$(document).ready(function() {
+  var widthOffset = 345,
+    heightOffset = 35,
+    d = new DataConverter('converter'),
+    sidebar = $('#header'),
+    win = $(window),
+    w = win.width() - widthOffset,
+    h = win.height() - heightOffset;
 
   d.create(w,h);
 
-  $(".settingsElement").change(updateSettings);
+  $('.settingsElement').change(updateSettings);
 
-  $(window).bind('resize',function() {  
+  $(window).resize(function() {
+    w = win.width() - widthOffset;
+    h = win.height() - heightOffset;
+    d.resize(w,h);
+    sidebar.height(h);
+  });
 
-      w = win.width() - widthOffset;
-      h = win.height() - heightOffset;
-      d.resize(w,h);
-      sidebar.height(h);
+  function updateSettings(evt) {
 
-    });
-
-
-  function updateSettings (evt) {
-    
     if (evt) {
-      _gaq.push(['_trackEvent', 'Settings',evt.currentTarget.id ]);
-    };
-
-    d.includeWhiteSpace = $('#includeWhiteSpaceCB').attr('checked');
-    
-    if (d.includeWhiteSpace) {
-      $("input[name=indentType]").removeAttr("disabled");
-      var indentType = $('input[name=indentType]:checked').val();
-      if (indentType === "tabs") {
-        d.indent = "\t";
-      } else if (indentType === "spaces") {
-        d.indent = "  "
-      }
-    } else {
-      $("input[name=indentType]").attr("disabled", "disabled");
+      _gaq.push(['_trackEvent', 'Settings', evt.currentTarget.id]);
     }
 
-    d.headersProvided = $('#headersProvidedCB').attr('checked');
+    d.includeWhiteSpace = $('#includeWhiteSpaceCB').prop('checked');
+
+    if (d.includeWhiteSpace) {
+      $('input[name=indentType]').removeAttr('disabled');
+      var indentType = $('input[name=indentType]:checked').val();
+      if (indentType === 'tabs') {
+        d.indent = '\t';
+      } else if (indentType === 'spaces') {
+        d.indent = '  ';
+      }
+    } else {
+      $('input[name=indentType]').prop('disabled', true);
+    }
+
+    d.headersProvided = $('#headersProvidedCB').prop('checked');
 
     if (d.headersProvided) {
-      $("input[name=headerModifications]").removeAttr("disabled");
+      $('input[name=headerModifications]').removeAttr('disabled');
 
       var hm = $('input[name=headerModifications]:checked').val();
-      if (hm === "downcase") {
+      if (hm === 'downcase') {
         d.downcaseHeaders = true;
         d.upcaseHeaders = false;
-      } else if (hm === "upcase") {
+      } else if (hm === 'upcase') {
         d.downcaseHeaders = false;
         d.upcaseHeaders = true;
-      } else if (hm === "none") {
+      } else if (hm === 'none') {
         d.downcaseHeaders = false;
         d.upcaseHeaders = false;
       }
     } else {
-      $("input[name=headerModifications]").attr("disabled", "disabled");
+      $('input[name=headerModifications]').prop('disabled', true);
     }
-    
+
     d.delimiter = $('input[name=delimiter]:checked').val();
     d.decimal = $('input[name=decimal]:checked').val();
-    
     d.useUnderscores = true;
-    
     d.convert();
-  };
+  }
 
   updateSettings();
-  
-})
 
+});
