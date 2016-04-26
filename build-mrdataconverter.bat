b35a1ee24b6939cbbd82332eacc46ad41186d1d8
Tom Doan <tomdoan@singpost.com>
2016-04-26 16:57:49 +0800

    Include build script

---
+++ build-mrdataconverter.bat
@@ -0,0 +1,4 @@
+@echo off
+:: Download AjaxMin.exe from http://ajaxmin.codeplex.com/releases
+AjaxMin.exe -clobber css\converter.css -out css\converter.min.css
+AjaxMin.exe -inline:no -clobber -term js\CSVParser.js js\DataGridRenderer.js js\Converter.js js\Controller.js -out js\app.js
